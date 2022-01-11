5086a9b8888ba52c628dcecbad793f331b267e61
umienatasha <uminatasha22@gmail.com>
2022-01-11 10:08:43 +0800

    Update regmaklumat.php

--- patient/regmaklumat.php
+++ patient/regmaklumat.php
@@ -10,21 +10,14 @@ $age=$_POST['age'];
 $phone=$_POST['phone'];
 $gender=$_POST['gender'];
 $comment=$_POST['comment'];
-$name2=$_POST['name2'];
-$email2=$_POST['email2'];
-$ic2=$_POST['ic2'];
-$age2=$_POST['age2'];
-$phone2=$_POST['phone2'];
-$gender2=$_POST['gender2'];
-$comment2=$_POST['comment2'];
 $id_patient=$_SESSION['id_patient'];
 
 $query = "SELECT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot' ";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $num_rows = mysqli_num_rows($result);
 if ($num_rows < 2) {
-	$sql = "INSERT INTO bookings (date, timeslot, name, email, ic, age, phone, gender, comment, name2, email2, ic2, age2, phone2, gender2, comment2, id_patient) 
-	VALUES ('$date', '$timeslot', '$name', '$email', '$ic','$age', '$phone', '$gender', '$comment', '$name2', '$email2', '$ic2', '$age2','$phone2', '$gender2', '$comment2', '$id_patient')";
+	$sql = "INSERT INTO bookings (date, timeslot, name, email, ic, age, phone, gender, comment, id_patient) 
+	VALUES ('$date', '$timeslot', '$name', '$email', '$ic','$age', '$phone', '$gender', '$comment', '$id_patient')";
 	echo "<div class='alert alert-success'>Tempahan Anda Berjaya !
 			
 			<script>
