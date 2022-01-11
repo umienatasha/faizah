12d7a7ee19d5c7b2b33a7a83e421948031f33d23
umienatasha <uminatasha22@gmail.com>
2022-01-11 12:46:32 +0800

    Update regmaklumat.php

--- patient/regmaklumat.php
+++ patient/regmaklumat.php
@@ -1,7 +1,6 @@
 <?php 
 include('connection.php');
 
-
 $date=$_POST['date'];
 $timeslot=$_POST['timeslot'];
 $name=$_POST['name'];
@@ -14,12 +13,13 @@ $comment=$_POST['comment'];
 
 $query = "SELECT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot' ";
 $id_book = $conn->insert_id;
+$_SESSION['id_book'] = $id_book;
 
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $num_rows = mysqli_num_rows($result);
 if ($num_rows < 2) {
-	$sql = "INSERT INTO bookings (date, timeslot, name, email, ic, age, phone, gender, comment) 
-	VALUES ('$date', '$timeslot', '$name', '$email', '$ic','$age', '$phone', '$gender', '$comment')";
+	$sql = "INSERT INTO bookings (date, timeslot, name, email, ic, age, phone, gender, comment, id_book) 
+	VALUES ('$date', '$timeslot', '$name', '$email', '$ic','$age', '$phone', '$gender', '$comment', '$id_book')";
 	
 	echo "<div class='alert alert-success'>Tempahan Anda Berjaya !
 			
@@ -29,7 +29,8 @@ if ($num_rows < 2) {
 			
 			</div>";
 			
-   }else{
+   }
+   else{
 	    echo "<div class='form'>
 			  <h3>Maaf, Tempahan Yang Anda Pilih Tidak Berjaya Atas Persesilihan Tempahan Dengan Pesakit Lain !</h3><br/>
 			  <p class='link'>Tekan <a href='slot.php'>Tempah</a> untuk menempah.</p>
