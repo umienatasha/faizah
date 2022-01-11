c34a2c59d48ef073c3b7fe46c57b51b23198b1a6
umienatasha <uminatasha22@gmail.com>
2022-01-11 15:12:11 +0800

    update

--- patient/regmaklumat.php
+++ patient/regmaklumat.php
@@ -14,14 +14,14 @@ $comment=$_POST['comment'];
 $query = "SELECT * FROM bookings WHERE date = '$date' AND timeslot = '$timeslot' ";
 $result = mysqli_query($conn, $query) or die(mysqli_error($conn));
 $num_rows = mysqli_num_rows($result);
-if ($num_rows < 2) {
+if ($num_rows < 1) {
 	$sql = "INSERT INTO bookings (date, timeslot, name, email, ic, age, phone, gender, comment)
 	VALUES ('$date', '$timeslot', '$name', '$email', '$ic','$age', '$phone', '$gender', '$comment')";
 	mysqli_query($conn, $sql) or die(mysqli_error($conn));
 	$id_book = $conn->insert_id;
 	$_SESSION['id_book'] = $id_book;
 	echo $_SESSION['id_book'];
-	echo $id_book; exit;
+	echo $id_book; 
 
 	echo "<div class='alert alert-success'>Tempahan Anda Berjaya !
 			
