a9bea3cea6c1553d244e40ee2de31b157d0caa1f
umienatasha <uminatasha22@gmail.com>
2022-01-11 11:48:52 +0800

    Update slot.php

--- patient/slot.php
+++ patient/slot.php
@@ -1,17 +1,5 @@
 <?php
 include('connection.php');
-if (!isset($_SESSION["id_patient"])) {
-	//  header("Location: login.php");
-	print_r($_SESSION);
-	exit();
-}
-
-$id_patient = $_SESSION['id_patient'];
-$sql = "SELECT  * FROM tblpatient WHERE id_patient='$id_patient' ";
-$result = mysqli_query($conn, $sql);
-
-$row = mysqli_fetch_assoc($result); // cara declare row
-$name = $row['name'];
 
 $times = ['09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '02:00 PM', '03:00 PM', '04:00 PM', '05:00 PM'];
 ?>
@@ -72,34 +60,57 @@ $times = ['09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '02:00 PM', '03:00 PM'
     </head>
     <body>
         <!--========== HEADER ==========-->
-        <header class="header">
+       <header class="header">
             <div class="header__container">
                 <img src="assets/img/4.jpg" alt="" class="header__img">
 
-                <a href="#" class="header__logo"><strong>Hai, <?php echo $name; ?></b> !</strong></a>
+                <a href="index.php" class="header__logo"><strong>Selamat Datang, Ohana Physiotherapy</b> !</strong></a>
 				
 				<div class="header__toggle">
                     <i class='bx bx-menu' id="header-toggle"></i>
                 </div>
+
             </div>
         </header>
 
-        <!--========== NAV ==========-->
+        <!--========== CONTENTS ==========-->
         <div class="nav" id="navbar">
             <nav class="nav__container">
                 <div>
-                    <a href="#" class="nav__link nav__logo">
+                    <a href="index.php" class="nav__link nav__logo">
                         <i class='bx bxs-disc nav__icon' ></i>
-                        <span class="nav__logo-name"><strong>Hai, <?php echo $name; ?></b> !</strong></span>
+                        <span class="nav__logo-name">Ohana Physiotherapy</span>
                     </a>
     
                     <div class="nav__list">
                         <div class="nav__items">
                             <h3 class="nav__subtitle">Profile</h3>
+    
+                            <a href="index.php" class="nav__link active">
+                                <i class='bx bx-user nav__icon' ></i>
+                                <span class="nav__name">Home</span>
+                            </a>
                             
-							<a href="profile.php" class="nav__link">
-								<i class='bx bx-user-pin nav__icon' ></i>
-								<span class="nav__name">Profile</span>
+							<div class="nav__dropdown">
+                                <a href="#" class="nav__link">
+                                    <i class='bx bx-category nav__icon' ></i>
+                                    <span class="nav__name">Tentang Kami</span>
+                                    <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i>
+                                </a>
+
+                                <div class="nav__dropdown-collapse">
+                                    <div class="nav__dropdown-content">
+                                        <a href="rawatan.php" class="nav__dropdown-item">Senarai Rawatan</a>
+                                        <a href="gallery.php" class="nav__dropdown-item">Galeri</a>
+                                        <a href="video.php" class="nav__dropdown-item">Testimoni</a>
+                                        <a href="time.php" class="nav__dropdown-item">Waktu Operasi</a>
+                                    </div>
+                                </div>
+                            </div>
+							
+							<a href="contact.php" class="nav__link">
+								<i class='bx bx-map-pin nav__icon' ></i>
+								<span class="nav__name">Lokasi</span>
 							</a>
 						</div>
     
@@ -107,56 +118,29 @@ $times = ['09:00 AM', '10:00 AM', '11:00 AM', '12:00 PM', '02:00 PM', '03:00 PM'
                             <h3 class="nav__subtitle">Managament</h3>
 							
 							<div class="nav__dropdown">
-                                <a href="#" class="nav__link active">
-                                    <i class='bx bx-calendar-heart nav__icon' ></i>
-                                    <span class="nav__name">Appointment</span>
+                                <a href="#" class="nav__link">
+                                    <i class='bx bx-file-find nav__icon' ></i>
+                                    <span class="nav__name">Temu Janji</span>
                                     <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i>
                                 </a>
 
                                 <div class="nav__dropdown-collapse">
                                     <div class="nav__dropdown-content">
-                                        <a href="slot.php" class="nav__dropdown-item">Booking</a>
-                                        <a href="viewbooking.php" class="nav__dropdown-item">Details</a>
+                                        <a href="search.php" class="nav__dropdown-item">Semak Temu Janji</a>
+                                        <a href="patient/slot.php" class="nav__dropdown-item">Tempah Slot</a>
                                     </div>
                                 </div>
                             </div>
 							
-							<div class="nav__dropdown">
-                                <a href="#" class="nav__link">
-                                    <i class='bx bx-list-plus nav__icon' ></i>
-                                    <span class="nav__name">Rawatan</span>
-                                    <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i>
-                                </a>
-
-                                <div class="nav__dropdown-collapse">
-                                    <div class="nav__dropdown-content">
-                                        <a href="viewtreatment.php" class="nav__dropdown-item">Details</a>
-                                    </div>
-                                </div>
-                            </div>
-							
-							<div class="nav__dropdown">
-                                <a href="#" class="nav__link">
-                                    <i class='bx bx-navigation nav__icon' ></i>
-                                    <span class="nav__name">Holiday</span>
-                                    <i class='bx bx-chevron-down nav__icon nav__dropdown-icon'></i>
-                                </a>
-
-                                <div class="nav__dropdown-collapse">
-                                    <div class="nav__dropdown-content">
-                                        <a href="viewcuti.php" class="nav__dropdown-item">Details</a>
-                                    </div>
-                                </div>
-                            </div>
+							<a href="cuti.php" class="nav__link">
+                                <i class='bx bx-navigation nav__icon' ></i>
+                                <span class="nav__name">Cuti Umum</span>
+                            </a>
 
                         </div>
                     </div>
                 </div>
 
-                <a href="../logout.php" class="nav__link nav__logout">
-                    <i class='bx bx-log-out nav__icon' ></i>
-                    <span class="nav__name">Log Out</span>
-                </a>
             </nav>
         </div>
 
